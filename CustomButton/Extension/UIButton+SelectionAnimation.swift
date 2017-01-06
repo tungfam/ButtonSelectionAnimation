9d5b213b6da961136d7769783d895fcb95840b8e
Tung Fam <tung@pixpie.co>
2017-01-06 15:27:15 +0200

    initial commit (implemented selection animation)

---
+++ CustomButton/Extension/UIButton+SelectionAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton+SelectionAnimation.swift
+//  CustomButton
+//
+//  Created by Tung Fam on 1/6/17.
+//  Copyright © 2017 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class UIButton_SelectionAnimation: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

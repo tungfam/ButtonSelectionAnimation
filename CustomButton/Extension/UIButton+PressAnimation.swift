abb1e9dc754488fbed541a550f9824f6cf3460cc
Tung Fam <tung@pixpie.co>
2017-01-07 13:36:02 +0200

    added 3 new buttons animation

---
+++ CustomButton/Extension/UIButton+PressAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton+PressAnimation.swift
+//  CustomButton
+//
+//  Created by Tung Fam on 1/7/17.
+//  Copyright © 2017 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class UIButton_PressAnimation: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
